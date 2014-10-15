04c49466472b3f85b3a455dc8f1c7d0994c18f55
Michael Rueegg <rueegg.michael@gmail.com>
2014-10-15 07:18:35 +0200

    Made console error pattern matcher more flexible for Windows Compiler

---
+++ ch.hsr.ifs.sconsolidator.core.tests/src/ch/hsr/ifs/sconsolidator/core/PlatformSpecificsTest.java
@@ -0,0 +1,44 @@
+package ch.hsr.ifs.sconsolidator.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.regex.Matcher;
+
+import org.junit.Test;
+
+public class PlatformSpecificsTest {
+
+  @Test
+  public void compilerErrorPatternMatchWithLinuxPath() {
+    Matcher m = PlatformSpecifics.CPP_RE.matcher("src/main.cpp(23)");
+    assertTrue(m.find());
+    assertEquals("src/main.cpp", m.group(1));
+    assertEquals("23", m.group(2));
+  }
+  
+  @Test
+  public void gccErrorPatternMatchWithLinuxPath() {
+    Matcher m = PlatformSpecifics.CPP_RE.matcher("src/shapes/Circle.cpp:6:28: error: ISO C++ forbids declaration of 'Circleasdf' with no type [-fpermissive]");
+    assertTrue(m.find());
+    assertEquals("src/shapes/Circle.cpp", m.group(1));
+    assertEquals("6", m.group(2));
+  }
+  
+  @Test
+  public void gccErrorPatternMatchWithLinuxPathAndWhitespace() {
+    Matcher m = PlatformSpecifics.CPP_RE.matcher("src/shapes/test with whitespace.h:3:1: error: 'vosdfid' does not name a type");
+    assertTrue(m.find());
+    assertEquals("src/shapes/test with whitespace.h", m.group(1));
+    assertEquals("3", m.group(2));
+  }
+  
+  @Test
+  public void compilerErrorPatternMatchWithWindowsPath() {
+    Matcher m = PlatformSpecifics.CPP_RE.matcher("platform\\windows\\bcSupport.cpp(48): error C2065: 'ipDes' : undeclared identifier");
+    assertTrue(m.find());
+    assertEquals("platform\\windows\\bcSupport.cpp", m.group(1));
+    assertEquals("48", m.group(2));
+  }
+  
+}
